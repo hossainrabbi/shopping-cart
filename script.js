c960ce2e59a63b2ce0ebcf85b8ed439e9aa84fe7
Hossain <hossainrabbi259@gmail.com>
2021-01-27 10:51:45 +0600

    phone value update

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const phoneButtonPlus = document.getElementById('phone-button-plus');
+const phoneButtonMinus = document.getElementById('phone-button-minus');
 
+// phone count plus
+let phoneValue = document.getElementById('phone-value').value;
+let phoneValueNumber = parseInt(phoneValue);
+//phone price
+const phonePrice = document.getElementById('phone-price').innerText;
+const phonePriceNumber = parseInt(phonePrice);
+
+// phoneButtonPlus event
+phoneButtonPlus.addEventListener('click', function () {
+  // phone count update
+  document.getElementById('phone-value').value = ++phoneValueNumber;
+  //phone price update
+  updatePhonePrice('phone-price');
+});
+
+// phoneButtonMinus event
+phoneButtonMinus.addEventListener('click', function () {
+  // phone count update
+  document.getElementById('phone-value').value = --phoneValueNumber;
+  //phone price update
+  updatePhonePrice('phone-price');
+});
+
+function updatePhonePrice(id) {
+  document.getElementById(id).innerText = phonePriceNumber * phoneValueNumber;
+}

1f9b7b94c55a76092bb038e156b8d50f3acceee9
Hossain <hossainrabbi259@gmail.com>
2021-01-28 22:11:40 +0600

    end all work

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-document
-  .getElementById('case-increment')
-  .addEventListener('click', function () {
-    const caseInput = document.getElementById('case-count');
-    const caseCount = parseInt(caseInput.value);
-    const caseNewCount = caseCount + 1;
-    caseInput.value = caseNewCount;
+function handleProductChange(product, isIncrement) {
+  const productInput = document.getElementById(product + '-count');
+  const productCount = parseInt(productInput.value);
+  let productNewCount = productCount;
+  if (isIncrement === true) {
+    productNewCount = productCount + 1;
+  }
+  if (isIncrement === false && productCount > 0) {
+    productNewCount = productCount - 1;
+  }
+  productInput.value = productNewCount;
+  let productTotal = 0;
+  if (product === 'phone') {
+    productTotal = productNewCount * 1219;
+  }
+  if (product === 'case') {
+    productTotal = productNewCount * 59;
+  }
+  document.getElementById(product + '-price').innerText = productTotal;
+  // call total calculate function
+  calculateTotal();
+}
 
-    const caseTotal = caseNewCount * 59;
-    document.getElementById('cover-price').innerText = caseTotal;
-  });
+function calculateTotal() {
+  const phoneCount = getCount('phone');
+  const caseCount = getCount('case');
+  const subtotal = phoneCount * 1219 + caseCount * 59;
+  document.getElementById('subtotal').innerText = subtotal;
+  // calculate product tax
+  const tax = Math.round(subtotal * 0.1);
+  document.getElementById('tax').innerText = tax;
+  // calculate product total amount
+  document.getElementById('total').innerText = subtotal + tax;
+}
 
-document
-  .getElementById('case-decrement')
-  .addEventListener('click', function () {
-    const caseInput = document.getElementById('case-count');
-    const caseCount = parseInt(caseInput.value);
-    const caseNewCount = caseCount - 1;
-    caseInput.value = caseNewCount;
-
-    const caseTotal = caseNewCount * 59;
-    document.getElementById('cover-price').innerText = caseTotal;
-  });
+// convert product value text to number
+function getCount(product) {
+  const productInput = document.getElementById(product + '-count');
+  return parseInt(productInput.value);
+}
